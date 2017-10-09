b2554c428a167a21085b86d4f6699f20418a9006
Michael Do <mdo3739@gmail.com>
2017-10-08 22:34:53 -0500

    Successfully changed favicon

--- src/config/devConfig.js
+++ src/config/devConfig.js
@@ -3,4 +3,5 @@ const keys = require('./keys');
 module.exports = function(app) {
 	process.env.PORT = 3000;
 	process.env.MONGODB = keys.MONGODB;
+	process.env.SECRET = keys.SECRET;
 };
--- src/server.js
+++ src/server.js
@@ -2,10 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const env = process.env.NODE_ENV || 'dev';
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const path = require('path');
 
+// Config
 if(env === 'dev'){
 	require('./config/devConfig')(app);
 }
+require('./config/passport')(passport);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Routes
+require('./userApi')(app);
+app.get('/', (req, res) => {
+	let markup = "HELLO WORLD";
+	res.render('index.ejs', {markup});
+});
 
 mongoose.connect(process.env.MONGODB, {useMongoClient: true}, function(err){
 	if(err) throw err;
